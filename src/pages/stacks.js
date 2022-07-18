bf5000ebab18ca3a4074942346e5fa3e3e2b9822
Sakina Tinwala <tinwalasakina515253@gmail.com>
2022-07-18 12:29:23 +0530

    - In Stack push, pop, and peek committed..

--- src/pages/stacks.js
+++ src/pages/stacks.js
@@ -4,66 +4,41 @@ import { useState } from "react";
 export const Stacks = () => {
     const [stackvalues, setStackValues] = useState ([1,2,3]);
 
+    const size = 5;
+    const push = (value) => {
+        if (stackvalues.length < size) {
+            setStackValues([...stackvalues, 4])
+        }
+    }
+    
+    const pop = () => {
+        if (stackvalues.length > 0) {
+            setStackValues(stackvalues.slice(0, -1))
+        }
+    }
+    
+    const peek = () => {
+        if (Stacks.length > 0) {
+        return stackvalues[Stacks.length - 1];
+        }
+    }
+    
     return (
         <div className="grid-rows-2 justify-center items-center">
             <div className="grid-cols-2 m-5">
-                <button onClick={() => setStackValues([...stackvalues, 4])}>
+                <button onClick={() => push()}>
                     Add to Stack..
                 </button>
             </div>
             <div className="grid-cols-2 m-5">
-                <button onClick={() => setStackValues(stackvalues.slice(0, -1))}>
+                <button onClick={() => pop()}>
                     Remove from Stack..
                 </button>
             </div>
             <div className="grid-cols-2 justify-center m-5">
                 <Stack values={stackvalues} />
+                {peek()}
             </div> 
         </div>
     )
 }
-
-
-/*
-const size = 3;
-
-const push = (value) => {
-    if (Stacks.length < size) {
-        Stacks.push(value);
-        return Stacks;
-    }
-    return 'Stacks is full ==> stackoverflow';
-}
-
-const pop = () => {
-    if (Stacks.length > 0) {
-        return Stacks.pop();
-    }
-    return 'Stacks is empty ==> underflow';
-}
-
-const peek = () => {
-    if (Stacks.length > 0) {
-        return Stacks[Stacks.length - 1];
-    }
-    return 'Stacks is empty ==> underflow';
-}
-
-push(1);
-console.log(Stacks);
-push(2);
-console.log(Stacks);
-push(3);
-console.log(Stacks);
-console.log(peek());
-push(4);
-console.log(Stacks);
-pop();
-console.log(Stacks);
-pop();
-console.log(Stacks);
-pop();
-console.log(Stacks);
-pop();
-console.log(Stacks);
-*/
